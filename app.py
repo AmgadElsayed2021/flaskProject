a9a9d874cf145f3ed350ea478520cc3a7a9b5ad4
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-11-27 16:09:29 -0330

    commit on 27 th nov

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
-
-app = Flask(__name__)
+from Application  import app
 
 
-@app.route('/')
-def hello_world() :  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__' :
-    app.run()
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "<h1>Amgad</h1>"
+#     # return render_template('index.html')
+#
+# # @app.route('/product')
+# # def product():
+# #     return render_template('view-product.html')
+# #
+# # @app.route('/cart')
+# # def cart():
+# #     return render_template('cart.html')
+# #
+# # @app.route('/checkout')
+# # def checkout():
+# #     return render_template('checkout.html')
+# #
+# # @app.route('/admin')
+# # def admin():
+# #     return render_template('admin/index.html', admin=True)
+# #
+# # @app.route('/admin/add')
+# # def add():
+# #     return render_template('admin/add-product.html', admin=True)
+# #
+# # @app.route('/admin/order')
+# # def order():
+# #     return render_template('admin/view-order.html', admin=True)
+# #
+# # if __name__ == '__main__':
+# #     app.run()
