a169e9d75906f25daaeefc6a9e58aec7a29e1608
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-11-28 00:46:16 -0330

    working on data base

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,6 +1,7 @@
+# here is where the application will run
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# lets import all the routes from the route.html
 from Application import routes
 
--- Application/routes.py
+++ Application/routes.py
@@ -1,20 +1,29 @@
+import sqlite3
 from Application import app
-from flask import render_template
+from flask import render_template,request
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 def index():
-    return render_template('index.html',login=True)
+    return render_template('index.html',index=True)# just make the index = true so i can use to style the nav button in the nav.html
+
 @app.route('/login')
 def login():
     return render_template('login.html',login=True)
 
 @app.route('/categories')
 def categories():
-    return render_template('categories.html',login=True)
+    return render_template('categories.html',categories=True)
 @app.route('/books')
 def books():
-    return render_template('books.html',login=True)
+    return render_template('books.html',books=True)
 @app.route('/register')
 def register():
-    return render_template('register.html',login=True)
+    return render_template('register.html',register=True)
