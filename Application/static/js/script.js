49921eb8925fce0649df9ac32a4f00d4167a64ae
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-11-29 16:07:12 -0330

    js for the registeration form is done

--- Application/static/js/script.js
+++ Application/static/js/script.js
@@ -9,6 +9,7 @@ $(document).ready(() => {
     // validate the name text
     let username=$("#username")
     const name =username.val().trim();
+    console.log(name)
     if (name === "") {
       username.next().text("This field is required.");
       isValid = false;
@@ -20,12 +21,12 @@ $(document).ready(() => {
     // validate the email1 input
     let email1=$("#email1")
     const email1Val = email1.val().trim();
-    if (email1Val === "") {
-      email1.next().text("This field is required.");
+    console.log(email1Val)
+   if (!emailPattern.test(email1Val)) {
+      // email1.next().text("Must be a valid email address.");
       isValid = false;
-    } else if (!emailPattern.test(email1)) {
       email1.next().text("Must be a valid email address.");
-      isValid = false;
+
     } else {
       email1.next().text("");
     }
@@ -34,13 +35,10 @@ $(document).ready(() => {
     // validate the email2 input
     let email2=$("#email2")
     const email2Val = email2.val().trim();
-    if (email2Val === "") {
-      email2.next().text("This field is required.");
-      isValid = false;
-    } else if (!emailPattern.test(email2)) {
+    if (!emailPattern.test(email2Val)) {
       email2.next().text("Must be a valid email address.");
       isValid = false;
-    } else if(email1Val!==email2Val){
+    } else if(email2Val !== email1Val){
        email2.next().text("emails doesn't match");
       isValid = false;
     }else {
@@ -51,10 +49,7 @@ $(document).ready(() => {
  // validate the password input
     let pass1=$("#pass1")
     const pass1val = pass1.val().trim();
-    if (pass1 == "") {
-      pass1.next().text("This field is required.");
-      isValid = false;
-    } else if (!passwordPattern.test(pass1)) {
+    if (!passwordPattern.test(pass1val)) {
       pass1.next().text("must include capital and small letters,number,special characters and the length between 6:64 ");
       isValid = false;
     } else {
@@ -65,13 +60,10 @@ $(document).ready(() => {
     // validate the password2 input
     let pass2=$("#pass2")
     const pass2val = pass2.val().trim();
-    if (pass2 == "") {
-      pass2.next().text("This field is required.");
-      isValid = false;
-    } else if (!passwordPattern.test(pass2)) {
+    if (!passwordPattern.test(pass2val)) {
       pass2.next().text("must include capital and small letters,number,special characters and the length between 6:64 ");
       isValid = false;
-    } else if(pass2!=pass1){
+    } else if(pass2val!==pass1val){
        pass2.next().text("password doesn't match");
       isValid = false;
     }else {
@@ -79,7 +71,7 @@ $(document).ready(() => {
     }
     pass2.val(pass2val);
 
-    if (isValid == false) {
+    if (isValid === false) {
       evt.preventDefault();
     }
   });
