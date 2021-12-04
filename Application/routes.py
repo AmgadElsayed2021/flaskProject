c7b2ea5a9b640c224d45c700f4cae65ff397d167
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-12-03 23:57:20 -0330

    working on some css styling for the main page

--- Application/routes.py
+++ Application/routes.py
@@ -13,6 +13,24 @@ conn.row_factory = sqlite3.Row
 @app.route('/index')
 def index():
     return render_template('index.html', index=True)# just make the index = true so i can use to style the nav button in the nav.html
+# msg = ''
+#     if request.method == 'POST' and 'username' in request.form and 'password' in request.form :
+#         username = request.form['username']
+#         password = request.form['password']
+#         with closing(conn.cursor()) as c :
+#             query=('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password,))
+#             c.execute(query)
+#         account = c.fetchone()
+#
+#         if account :
+#             session['loggedin'] = True
+#             session['id'] = account['id']
+#             session['username'] = account['username']
+#             msg = 'Logged in successfully !'
+#             return render_template('index.html', msg=msg)
+#         else :
+#             msg = 'Incorrect username / password !'
+#     return render_template('login.html', msg=msg)
 
 @app.route('/login')
 def login():
@@ -29,9 +47,8 @@ def categories():
             categories.append(result)
 
 
-
         query = 'select * from Books where category=?'
-        c.execute(query,({{ form.selected}},))
+        c.execute(query,({{ categories}},))
         results = c.fetchall()
         category_books = []
         for result in results :
