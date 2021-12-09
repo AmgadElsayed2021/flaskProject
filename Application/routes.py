3f6128d8cd6d013a1eb1638a6daa667fb0d3b3fd
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2021-12-08 21:59:41 -0330

    last commit today

--- Application/routes.py
+++ Application/routes.py
@@ -2,9 +2,12 @@ import os
 import sqlite3
 from Application import app
 from Application.forms import LoginForm
-from flask import render_template, request, session, redirect, flash
+from flask import render_template, request, session, redirect, flash, current_app, abort, url_for
 from contextlib import closing
 import os
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg', '.jfif']
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -19,18 +22,23 @@ app.config['UPLOAD_PATH'] = 'Application/static/images'
 @app.route('/home')
 @app.route('/index')
 def index() :
+    if session.get("email"):
+        return redirect(url_for('index'))
     return render_template('index.html')
 
-
+# here is the route for the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login() :
+    if session.get("email"):
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit() :
         if request.method == 'POST' and 'email' in request.form and 'password' in request.form :
             email = request.form['email']
             password = request.form['password']
             with closing(conn.cursor()) as c :
-                c.execute('SELECT * FROM accounts WHERE email = ? AND password = ?', (email, password,))
+                c.execute('SELECT * FROM Accounts WHERE email = ? AND password = ?', (email, password,))
                 account = c.fetchone()
 
             if account :
@@ -39,16 +47,30 @@ def login() :
                 session['email'] = account['email']
 
                 # use flash and bootstrap to display a message with success style
-                flash(f'you are successfully logged in!',"success")
+                flash(f'you are successfully logged in!', "success")
                 return redirect('index')
             else :
                 # use flash and bootstrap to display a message  with danger style
-                flash("sorry : something went wrong.","danger")
-    return render_template('login.html', title="Login", form=form, login=True)
+                flash("sorry : something went wrong.", "danger")
+    return render_template('login.html', title="Login", form=form, login=False)
+
+@app.route("/logout")
+def logout():
+    session['email']=False
+    session.pop('email',None)
+    return redirect(url_for('index'))
 
 
+
+
+
+# route for category books
 @app.route('/categoryBooks', methods=['GET', 'POST'])
 def categoryBooks() :
+    if not session.get("email"):
+        return redirect(url_for('login'))
+    email=session.get['email']
+    # query to search the data base for all the available books for a specific category
     category = request.args.get('category')
     with closing(conn.cursor()) as c :
         c.execute("SELECT  * FROM Books WHERE category=?", (category,))
@@ -61,7 +83,7 @@ def categoryBooks() :
 
 @app.route('/categories', methods=['GET', 'POST'])
 def categories() :
-    # images=['murach java.png','pacific.jpg']
+    #  query the data base to get all the categories
     with closing(conn.cursor()) as c :
         c.execute("SELECT DISTINCT  category ,COUNT (*) AS nOfBooks FROM Books GROUP BY category")
         results = c.fetchall()
@@ -73,7 +95,7 @@ def categories() :
 
 @app.route('/books')
 def books() :
-    # images=['murach java.png','pacific.jpg']
+    # query the database for all the books that is available in it
     with closing(conn.cursor()) as c :
         c.execute("SELECT * FROM Books")
         results = c.fetchall()
@@ -86,11 +108,14 @@ def books() :
 # below is where the registration routes will be coded
 @app.route('/register')
 def register() :
+    if session.get("email"):
+        return redirect(url_for('index'))
     return render_template('register.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def getRegistrationFormData() :
+    # get the information that is given through the registration form
     fname = request.form['fname']
     lname = request.form['lname']
     email = request.form['email']
@@ -110,20 +135,28 @@ def update() :
 
 @app.route('/update', methods=['POST', 'GET'])
 def getFormData() :
+    #  get all the data from the update books form and add them to the data base
     title = request.values['title']
     year = request.values['year']
     description = request.values['description']
     category = request.values['category']
     uploaded_file = request.files['file']
-
+    # check if the uploaded file match the specific requirement we set on top of this routes.py
     if uploaded_file.filename != '' :
+        file_ext = os.path.splitext(uploaded_file.filename)[1]
+        if file_ext not in current_app.config['UPLOAD_EXTENSIONS'] :
+            abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], uploaded_file.filename))
+        # now lets add all those data to the data base
         with closing(conn.cursor()) as c :
             c.execute('INSERT INTO Books (Title,Description,Year,Image,Category) VALUES (?,?,?,?,?);',
                       (title, description, year, uploaded_file.filename, category))
             conn.commit()
     return redirect('books')
 
+
+
+#  code the deleting route below here
 
 @app.route('/delete')
 def delete() :
@@ -132,6 +165,7 @@ def delete() :
 
 @app.route('/delete', methods=['POST', 'GET'])
 def getDeletedData() :
+    # get the book title from the delete book form and use this in a query to delete that book from the data base
     title = request.values['title']
     with closing(conn.cursor()) as c :
         c.execute('Delete from Books where Title=? ;', (title,))
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object) :
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'AmgadAbbasEricStock12345678@#$%^&*()Elsayed'
     
